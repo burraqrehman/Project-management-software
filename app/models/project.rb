acf791f7414fc9f526cf0b6dd85c58c537e93947
Burraq <mianburraq813@gmail.com>
2021-06-23 12:33:52 +0500

    Add J Query and Improve Remote true

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,14 +2,15 @@ class Project < ApplicationRecord
 
 	belongs_to :user
 
-	has_many :comments
+	has_many :comments, dependent: :destroy
 	
 	has_one_attached :image
 
 	validates :name, presence: true, uniqueness: { case_sensitive: false }
 	validates :documentation, presence: true
-	validates :project_cost, presence: true
-	validates :time_tracking, presence: true, numericality: true
-	validates :image, presence: true, numericality: true
+	validates :project_cost, presence: true, numericality: true
+	validates :time_tracking, presence: true
+	validates :image, presence: true
+
 
 end
