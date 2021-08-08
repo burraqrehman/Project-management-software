d7652b3ac51d433629c571d69b19ae693b42d494
Burraq <mianburraq813@gmail.com>
2021-08-08 17:35:25 +0500

    Adding new feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,29 @@
-class UsersController < ApplicationController
+# frozen_string_literal: true
 
-	def show
-		@user = User.find(params[:id])
-	end
-	
+class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update]
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit; end
+
+  def update
+    if @user.update(user_params)
+      redirect_to [:admin, @user], notice: 'User was successfully created.'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :contact, :password, :password_confirmation)
+  end
 end
--- app/javascript/packs/payment.js
+++ app/javascript/packs/payment.js
@@ -1,15 +1,15 @@
-$(document).ready(function(){
-  $(‘#payment_client_id’).on(“change”, function(e) {
-    var project_id = $(this).val();
-    var url = “/manager/clients/” + project_id + “/projects”
-    $.ajax ({
-      type: ‘GET’,
-      url: ‘url’,
-      dataType: ‘json’,
-      data: {project: e.target_id},
-      success: function(data){
-        debugger
-      }
-    })
-  })
-})
+// $(document).ready(function(){
+//   $(‘#payment_client_id’).on(“change”, function(e) {
+//     var project_id = $(this).val();
+//     var url = “/manager/clients/” + project_id + “/projects”
+//     $.ajax ({ 
+//       type: ‘GET’,
+//       url: ‘/manager/clients/” + project_id + “/projects’,
+//       dataType: ‘json’,
+//       data: {project: e.target_id},
+//       success: function(data){
+//         debugger
+//       }
+//     })
+//   })
+// })
